ae3c947ef4e5950a01f163568e29c937e3c1d2e7
Vole1 <201406223@post.au.dk>
2018-11-02 20:38:53 +0100

    Vole1 - comm ex7

--- Exercise7/file_server.py
+++ Exercise7/file_server.py
@@ -2,31 +2,32 @@ import sys
 from socket import *
 
 
-HOST = '10.0.0.1' 	#servers IP-adresse
-BUFSIZE = 1000 		#Buffer-størrelse
+HOST = '10.0.0.1' 	
+BUFSIZE = 1000 		
 UPTIME = "/proc/uptime"	
 LOADAVG = "/proc/loadavg"
+PORT = 9000
 def main(argv):
-	#Initialiserer socket
+
 	sock = socket(AF_INET, SOCK_DGRAM)
 	sock.bind((HOST, PORT))
 	print("Server er klar")
 	
-	#Programløkke
+	
 	while True:
-		#Modtager data fra socket
+		
 		msg, addr = sock.recvfrom(BUFSIZE)
 		
 		print("Kommando modtaget: %s" % msg)
 		
-		#Sender værdi fra uptime
+		
 		if msg == "u" or msg == "U":
 			file = open(UPTIME)
 			data = file.read(BUFSIZE)
 			bytesSendt = sock.sendto(data, addr)
 			print("%s bytes returneret til %s" % (bytesSendt, addr))
 		
-		#Eller sender værdi fra loadavg
+		
 		elif msg == "l" or msg == "L":
 			file = open(LOADAVG)
 			data = file.read(BUFSIZE)
