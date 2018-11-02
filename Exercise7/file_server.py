ce8d29939cd45dff0c94f3163513df7a7524e8e9
Vole1 <201406223@post.au.dk>
2018-11-02 20:21:08 +0100

    ex7 comm

--- Exercise7/file_server.py
+++ Exercise7/file_server.py
@@ -2,28 +2,36 @@ import sys
 from socket import *
 
 
-HOST = '10.0.0.1'
-PORT = 9000
-BUFSIZE = 1000
-UPTIME = "/proc/uptime"
+HOST = '10.0.0.1' 	#servers IP-adresse
+BUFSIZE = 1000 		#Buffer-størrelse
+UPTIME = "/proc/uptime"	
 LOADAVG = "/proc/loadavg"
 def main(argv):
+	#Initialiserer socket
 	sock = socket(AF_INET, SOCK_DGRAM)
-	servSock.bind((HOST, PORT))
+	sock.bind((HOST, PORT))
+	print("Server er klar")
 	
+	#Programløkke
 	while True:
+		#Modtager data fra socket
 		msg, addr = sock.recvfrom(BUFSIZE)
 		
-		print(msg)
-	
+		print("Kommando modtaget: %s" % msg)
+		
+		#Sender værdi fra uptime
 		if msg == "u" or msg == "U":
 			file = open(UPTIME)
 			data = file.read(BUFSIZE)
-			sock.sendto(data, addr)
-		elif data == "l" or data == "L":
+			bytesSendt = sock.sendto(data, addr)
+			print("%s bytes returneret til %s" % (bytesSendt, addr))
+		
+		#Eller sender værdi fra loadavg
+		elif msg == "l" or msg == "L":
 			file = open(LOADAVG)
 			data = file.read(BUFSIZE)
-			sock.sendto(data, addr)
+			bytesSendt = sock.sendto(data, addr)
+			print("%s bytes returneret til %s" % (bytesSendt, addr))
 			
 		else:
 			print("Invalid command")
