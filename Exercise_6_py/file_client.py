19bf557e624ee4cf0f7c13f2c816411e007e341d
vole2 <201406223@post.au.dk>
2018-11-02 21:33:08 +0100

    Vole2: helt done

--- Exercise_6_py/file_client.py
+++ Exercise_6_py/file_client.py
@@ -4,12 +4,16 @@ from lib import Lib
 
 PORT = 9000
 BUFSIZE = 1000
-SERVER = '10.0.0.1'
 
 def main(argv):
+	if len(argv) > 0:
+		SERVER = argv[0]
+	else:
+		print("Benyt formatet 'python file_client.py <IP>'\n")
+		sys.exit()
 	sock = socket(AF_INET, SOCK_STREAM)
 	sock.connect((SERVER,PORT))
-	print("Forbundet til server")
+	print("Forbundet til server\n")
 	cmd = input("Indtast filnavn. Ved afslutning skriv 'exit'\n")
 	while cmd != "exit":
 		Lib.writeTextTCP(cmd,sock)
@@ -20,8 +24,9 @@ def main(argv):
 		if fileSize > 0:
 			receiveFile(cmd, sock, fileSize)
 		else:
-			print("Fil ikke fundet")
+			print("Fil ikke fundet\n")
 		cmd = input("Indtast filnavn. Ved afslutning skriv 'exit'\n")
+	Lib.writeTextTCP(cmd,sock)
 	sock.close()
 	sys.exit()
 		
@@ -35,7 +40,7 @@ def receiveFile(nm,  conn, siz):
 		buff = conn.recv(BUFSIZE)
 		file.write(buff)
 		bytesRead = bytesRead+len(buff)
-	print("Transfer complete")
+	print("Transfer complete\n")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
